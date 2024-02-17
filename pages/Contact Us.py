daea7967ae5cd9c1eccf3a3531fb4653aa07e7ff
Elena Daurtseva <diosama99@gmail.com>
2024-02-17 20:11:48 +0700

    add script for sending emails

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form("уьфшд_form"):
+    user_mail = st.text_input("Your email")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    print(button)
+    if button:
+        print('I was pressed')
+        print(button)
